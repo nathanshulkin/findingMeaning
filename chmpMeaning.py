47035721461a4a08942d0f5a2e9d24e66bad0cdc
nathanshulkin <nathanshulkin@uccs.edu>
2020-12-13 15:54:01 -0700

    Updates updatezz

--- chmpMeaning.py
+++ chmpMeaning.py
@@ -15,4 +15,8 @@ with open("chLg.json", 'r') as chSon:
     chLg20 = json.load(chSon)
 
 for team in chLg20:
+    print('\n\n')
     print(team)
+    for gameday in chLg20[team]:
+        for scorer in chLg20[team][gameday]['goals']:
+            print(scorer + ": " + str(chLg20[team][gameday]['goals'][scorer]))
